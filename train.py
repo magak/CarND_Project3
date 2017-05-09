6cfaeaea25842f1b572447866616c5f70da3c611
magak <magakk.stepanyan@gmail.com>
2017-05-09 13:11:11 +0000

    first chan

--- train.py
+++ train.py
@@ -2,8 +2,14 @@ import csv
 import cv2
 import numpy as np
 
+
+#path = 'my_data'
+path = 'data/data'
+#delimeter = '\\'
+delimeter = '/'
+
 lines = []
-with open('../my_data/driving_log.csv') as csvfile:
+with open('../'+path+'/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     # skip the header
     next(reader, None)
@@ -14,25 +20,27 @@ images = []
 measurements = []
 for line in lines:
     source_path = line[0]
-    filename = source_path.split('/')[-1]
-    current_path = '../my_data/IMG/' + filename
+    filename = source_path.split(delimeter)[-1]
+    current_path = '../'+path+'/IMG/' + filename
     image = cv2.imread(current_path)
     images.append(image)
     measurement = float(line[3])
     measurements.append(measurement)
 
+print(current_path)
 X_train = np.array(images)
 y_train = np.array(measurements)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Lambda
 
 model = Sequential()
-model.add(Flatten(input_shape=(160,320,3)))
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
+model.add(Flatten())
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer ='adam')
 #print(X_train[6])
 #print(current_path)
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=1)
 model.save('model.h5')
