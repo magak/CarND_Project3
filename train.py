4e545c2db1f17d233755235b7951500e10375479
magak <magak@rambler.ru>
2017-05-11 08:18:02 +0300

    nvidia net

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import csv
 import cv2
 import numpy as np
-
+from sklearn.utils import shuffle
 
 #path = 'my_data'
 path = 'data/data'
@@ -27,20 +27,45 @@ for line in lines:
     measurement = float(line[3])
     measurements.append(measurement)
 
-print(current_path)
+####################################
+# augmenting by flipping
+augmented_images, augmented_measurements = [], []
+for image, measurement in zip(images, measurements):
+    augmented_images.append(image)
+    augmented_measurements.append(measurement)
+    augmented_images.append(cv2.flip(image,1))
+    augmented_measurements.append(measurement*-1.0)
+####################################
+
 X_train = np.array(images)
 y_train = np.array(measurements)
 
+#Shuffle the training data
+X_train, y_train = shuffle(X_train, y_train)
+
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Convolution2D
 
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((70,25),(0,0))))
+
+model.add(Convolution2D(24,5,5,subsample=(2,2),activation="relu"))
+model.add(Convolution2D(36,5,5,subsample=(2,2),activation="relu"))
+model.add(Convolution2D(48,5,5,subsample=(2,2),activation="relu"))
+
+model.add(Convolution2D(64,3,3,activation="relu"))
+model.add(Convolution2D(64,3,3,activation="relu"))
+
 model.add(Flatten())
+
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer ='adam')
 #print(X_train[6])
 #print(current_path)
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=1)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=3)
 model.save('model.h5')
