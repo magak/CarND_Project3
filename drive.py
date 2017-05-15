92c131cd36887a9d8808919fbaa9ed5c1713b02e
magak <magak@rambler.ru>
2017-05-16 01:34:47 +0300

    with model.py

--- drive.py
+++ drive.py
@@ -3,7 +3,7 @@ import base64
 from datetime import datetime
 import os
 import shutil
-import preproc as prc
+import cv2
 
 import numpy as np
 import socketio
@@ -62,7 +62,8 @@ def telemetry(sid, data):
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
         image_array = np.asarray(image)
-        image_array = prc.preproc(image_array).reshape(160,320,1)
+        # grayscale conversation
+        image_array = cv2.cvtColor(image_array, cv2.COLOR_BGR2GRAY).reshape(160,320,1)
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
 
         throttle = controller.update(float(speed))
---
+++ model.py
@@ -0,0 +1,128 @@
+path = 'my_data/1205'
+delimeter = '\\'
+
+def getImagesFromLines(line):
+    #
+    # Getting images for line in file driving_log.csv
+    #
+
+    # center camera image
+    centerImgPath = line[0]    
+    filename = centerImgPath.split(delimeter)[-1]
+    center_path = '../'+path+'/IMG/' + filename
+    # grayscale conversion
+    centerImg = cv2.cvtColor(cv2.imread(center_path), cv2.COLOR_BGR2GRAY)
+
+    # left camera image
+    leftImgPath = line[1]
+    filename = leftImgPath.split(delimeter)[-1]
+    left_path = '../'+path+'/IMG/' + filename
+    # grayscale conversion
+    leftImg = cv2.cvtColor(cv2.imread(left_path), cv2.COLOR_BGR2GRAY)
+
+    # right camera image
+    rightImgPath = line[2]
+    filename = rightImgPath.split(delimeter)[-1]
+    right_path = '../'+path+'/IMG/' + filename
+    # grayscale conversion
+    rightImg = cv2.cvtColor(cv2.imread(right_path), cv2.COLOR_BGR2GRAY)
+
+    return centerImg, leftImg, rightImg
+
+import csv
+import cv2
+import numpy as np
+import sklearn
+from sklearn.utils import shuffle
+
+
+#############################################################################################################
+
+# read lines from driving_log.csv
+lines = []
+with open('../'+path+'/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    # skip the header
+    next(reader, None)
+    for line in reader:
+        lines.append(line)
+
+# splitting data for train and validation set
+from sklearn.model_selection import train_test_split
+train_samples, validation_samples = train_test_split(lines, test_size=0.2)
+
+# generator for fetching images for the model
+def generator(samples, batch_size=32):
+    # correction for left and right cameras
+    correction = 0.4
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+
+            images = []
+            angles = []
+            augmented_images = []
+            augmented_measurements = []
+            for batch_sample in batch_samples:
+                
+                centerImg, leftImg, rightImg = getImagesFromLines(batch_sample)            
+                measurement = float(batch_sample[3])
+    
+                images.append(centerImg)
+                angles.append(measurement)
+
+                # left image goes with + correction
+                images.append(leftImg)
+                angles.append(measurement+correction)
+
+                # right image goes with - correction
+                images.append(rightImg)
+                angles.append(measurement-correction)
+                
+                # augmenting by flipping images
+                augmented_images, augmented_measurements = [], []
+                for image, measurement in zip(images, angles):
+                    augmented_images.append(image.reshape(160,320,1))
+                    augmented_measurements.append(measurement)
+                    augmented_images.append(cv2.flip(image,1).reshape(160,320,1))
+                    augmented_measurements.append(measurement*-1.0)
+            
+            X_train = np.array(augmented_images)
+            y_train = np.array(augmented_measurements)
+            yield sklearn.utils.shuffle(X_train, y_train)
+
+# generators for train and validation sets
+train_generator = generator(train_samples, batch_size=128)
+validation_generator = generator(validation_samples, batch_size=128)
+
+#############################################################################################################
+
+
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Convolution2D, Dropout
+
+model = Sequential()
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,1)))
+# trim image to only see section with road
+model.add(Cropping2D(cropping=((70,25),(0,0))))
+
+model.add(Convolution2D(36,5,5,subsample=(2,2),activation="relu"))
+model.add(Convolution2D(48,5,5,subsample=(2,2),activation="relu"))
+model.add(Convolution2D(60,5,5,subsample=(2,2),activation="relu"))
+
+model.add(Convolution2D(76,3,3,activation="relu"))
+model.add(Convolution2D(76,3,3,activation="relu"))
+
+model.add(Flatten())
+
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer ='adam')
+model.fit_generator(train_generator, samples_per_epoch=len(train_samples)*6 , validation_data=validation_generator,nb_val_samples=len(validation_samples)*6, nb_epoch=2)
+model.save('model.h5')
