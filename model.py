82aab7a93c44d5063b21841b5b064a76986ace80
magak <magakk.stepanyan@gmail.com>
2017-05-16 00:40:05 +0000

    3 epochs

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-path = 'my_data/1205'
+path = '/dev/shm/my_data/16.05'
 delimeter = '\\'
 
 def getImagesFromLines(line):
@@ -9,21 +9,21 @@ def getImagesFromLines(line):
     # center camera image
     centerImgPath = line[0]    
     filename = centerImgPath.split(delimeter)[-1]
-    center_path = '../'+path+'/IMG/' + filename
+    center_path = path+'/IMG/' + filename
     # grayscale conversion
     centerImg = cv2.cvtColor(cv2.imread(center_path), cv2.COLOR_BGR2GRAY)
 
     # left camera image
     leftImgPath = line[1]
     filename = leftImgPath.split(delimeter)[-1]
-    left_path = '../'+path+'/IMG/' + filename
+    left_path = path+'/IMG/' + filename
     # grayscale conversion
     leftImg = cv2.cvtColor(cv2.imread(left_path), cv2.COLOR_BGR2GRAY)
 
     # right camera image
     rightImgPath = line[2]
     filename = rightImgPath.split(delimeter)[-1]
-    right_path = '../'+path+'/IMG/' + filename
+    right_path = path+'/IMG/' + filename
     # grayscale conversion
     rightImg = cv2.cvtColor(cv2.imread(right_path), cv2.COLOR_BGR2GRAY)
 
@@ -40,7 +40,7 @@ from sklearn.utils import shuffle
 
 # read lines from driving_log.csv
 lines = []
-with open('../'+path+'/driving_log.csv') as csvfile:
+with open(path+'/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     # skip the header
     next(reader, None)
@@ -51,7 +51,7 @@ with open('../'+path+'/driving_log.csv') as csvfile:
 from sklearn.model_selection import train_test_split
 train_samples, validation_samples = train_test_split(lines, test_size=0.2)
 
-# generator for fetching images for the model
+# generator for fetching images for the model instead of keeping big data in memory
 def generator(samples, batch_size=32):
     # correction for left and right cameras
     correction = 0.4
@@ -105,7 +105,7 @@ from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D, Convolution2D, Dropout
 
 model = Sequential()
-model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,1)))
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,1))) # data normalization
 # trim image to only see section with road
 model.add(Cropping2D(cropping=((70,25),(0,0))))
 
@@ -124,5 +124,5 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer ='adam')
-model.fit_generator(train_generator, samples_per_epoch=len(train_samples)*6 , validation_data=validation_generator,nb_val_samples=len(validation_samples)*6, nb_epoch=2)
+model.fit_generator(train_generator, samples_per_epoch=len(train_samples)*6 , validation_data=validation_generator,nb_val_samples=len(validation_samples)*6, nb_epoch=3)
 model.save('model.h5')
