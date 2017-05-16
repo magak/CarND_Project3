3d76e708e2b11d07ddad54e593f80d2220eb9796
magak <magakk.stepanyan@gmail.com>
2017-05-16 12:16:54 +0000

    droput with less conv filters

--- model.py
+++ model.py
@@ -109,16 +109,16 @@ model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,1))) # data nor
 # trim image to only see section with road
 model.add(Cropping2D(cropping=((70,25),(0,0))))
 
+model.add(Convolution2D(24,5,5,subsample=(2,2),activation="relu"))
+model.add(Dropout(0.2))
 model.add(Convolution2D(36,5,5,subsample=(2,2),activation="relu"))
 model.add(Dropout(0.2))
 model.add(Convolution2D(48,5,5,subsample=(2,2),activation="relu"))
 model.add(Dropout(0.2))
-model.add(Convolution2D(60,5,5,subsample=(2,2),activation="relu"))
-model.add(Dropout(0.2))
 
-model.add(Convolution2D(76,3,3,activation="relu"))
+model.add(Convolution2D(64,3,3,activation="relu"))
 model.add(Dropout(0.2))
-model.add(Convolution2D(76,3,3,activation="relu"))
+model.add(Convolution2D(64,3,3,activation="relu"))
 model.add(Dropout(0.2))
 
 model.add(Flatten())
